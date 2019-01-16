8c8345414c6fa474cbd23c311748f9cb18ccf552
OLskrain <Olskrain2@yandex.ru>
2019-01-16 23:30:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/olskr/cplhm2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.olskr.cplhm2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding3.widget.RxTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.edit_text1)
+    EditText editText;
+    @BindView(R.id.text_view1)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+}
